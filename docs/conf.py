50a9d6bccc32cc901d07e3dce2a8569ccb8a8fed
TPYBoard <info@turnipsmart.com>
2017-05-16 18:35:44 +0800

    document

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-
+from collections import OrderedDict
 html_context = {
     "display_github": True, # Integrate GitHub
     "github_user": "MyUserName", # Username
